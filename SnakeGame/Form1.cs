0124b97bad5e5048b17005c31fef4165dab89b73
SimonWWWW <winklarz.szymon@gmail.com>
2021-04-25 18:39:07 +0200

    Form1 update

--- SnakeGame/Form1.cs
+++ SnakeGame/Form1.cs
@@ -12,14 +12,16 @@ namespace SnakeGame
 {
     public partial class Form1 : Form
     {
+        private List<Circle> Snake = new List<Circle>(); //creating an list array for the snake
+        private Circle food = new Circle(); //creating a single Circle class called food
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-
+            new Settings(); //linking the Settings CLass to this Form
+            gameTimer.Interval = 1000 / Settings.Speed; // changing the game time to settings speed
+            gameTimer.Tick += updateScreen; //linking a updateScreen function to the timer
+            gameTimer.Start(); //starting the timer
+            startGame();
         }
 
         private void keyisdown(object sender, KeyEventArgs e)
@@ -33,6 +35,62 @@ namespace SnakeGame
         }
 
         private void updateGraphics(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void updateScreen(object sender, EventArgs e)
+        {
+            //timers update screen function
+            if(Settings.GameOver == true)
+            {
+                //if the tame over is true and players presses enter we run the start game function
+                if (Input.KeyPress(Keys.Enter))
+                {
+                    startGame();
+                }
+            }
+            else
+            {
+                //if the game is not over then the following commands will be executed
+                //below the actions will probe the keys being pressed by the player
+                //and move the accoridingly
+                if(Input.KeyPress(Keys.Right) && Settings.direction != Directions.Left)
+                {
+                    Settings.direction = Directions.Right;
+                }
+                else if (Input.KeyPress(Keys.Left) && Settings.direction != Directions.Right)
+                {
+                    Settings.direction = Directions.Left;
+                }
+                else if (Input.KeyPress(Keys.Up) && Settings.direction != Directions.Down)
+                {
+                    Settings.direction = Directions.Up;
+                }
+                else if (Input.KeyPress(Keys.Down) && Settings.direction != Directions.Up)
+                {
+                    Settings.direction = Directions.Down;
+                }
+                movePlayer(); //run move player function
+            }
+            pbCanvas.Invalidate(); //refresh the picture box and update graphics on it
+        }
+        private void startGame()
+        {
+
+        }
+        private void movePlayer()
+        {
+
+        }
+        private void generateFood()
+        {
+
+        }
+        private void eat()
+        {
+
+        }
+        private void die()
         {
 
         }
