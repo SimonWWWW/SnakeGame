814fa2782e9cf805d0e1db208363466b15dd20ea
SimonWWWW <winklarz.szymon@gmail.com>
2021-04-25 18:51:02 +0200

    Form1 update

--- SnakeGame/Form1.cs
+++ SnakeGame/Form1.cs
@@ -80,7 +80,55 @@ namespace SnakeGame
         }
         private void movePlayer()
         {
-
+            for (int i = Snake.Count -1; i>= 0; i--)
+            {
+                if(i == 0)
+                {
+                    switch (Settings.direction)
+                    {
+                        case Directions.Right:
+                            Snake[i].X++;
+                            break;
+                        case Directions.Left:
+                            Snake[i].X--;
+                            break;
+                        case Directions.Up:
+                            Snake[i].Y--;
+                            break;
+                        case Directions.Down:
+                            Snake[i].Y++;
+                            break;
+                    }
+                    int maxXpos = pbCanvas.Size.Width / Settings.Width;
+                    int maxYpos = pbCanvas.Size.Height / Settings.Height;
+                    if (
+                        Snake[i].X < 0 || Snake[i].Y < 0 || 
+                        Snake[i].X > maxXpos || Snake[i].Y > maxXpos)
+                    {
+                        //end the game is snake either reaches edge of the canvas
+                        die();
+                    }
+                    //detect collision with the body
+                    for(int j = 1; j < Snake.Count; j++)
+                    {
+                        if(Snake[i].X == Snake[j].X && Snake[i].Y == Snake[j].Y)
+                        {
+                            die();
+                        }
+                    }
+                    //detect collision between snake head and food
+                    if(Snake[0].X == food.X && Snake[0].Y == food.Y)
+                    {
+                        eat(); // run eat function
+                    }
+                }
+                else
+                {
+                    //if there are no collisions then we continue moving the snake 
+                    Snake[i].X = Snake[i - 1].X;
+                    Snake[i].Y = Snake[i - 1].Y;
+                }
+            }
         }
         private void generateFood()
         {
